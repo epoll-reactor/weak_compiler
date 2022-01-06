c9614fc5464213dd8dc8cbc5ca39fa29edd7716d
epoll-reactor <>
2022-01-06 11:13:22 +0100

    Implement semantics of storing unique links for CFG block.

--- compiler/include/Utility/Uncopyable.hpp
+++ compiler/include/Utility/Uncopyable.hpp
@@ -9,6 +9,8 @@ namespace weak {
 class Uncopyable {
 public:
   Uncopyable() = default;
+  Uncopyable(Uncopyable &&) = default;
+  Uncopyable &operator=(Uncopyable &&) = default;
   Uncopyable(const Uncopyable &) = delete;
   Uncopyable &operator=(const Uncopyable &) = delete;
 };
