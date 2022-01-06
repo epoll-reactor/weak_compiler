c033eced0acdb3e9f76aa6c5e9de836f5d36b261
epoll-reactor <>
2022-01-06 15:10:15 +0100

    Allow only 1 or 2 successors in CFG nodes.

--- compiler/src/FrontEnd/Analysis/CFGBlock.cpp
+++ compiler/src/FrontEnd/Analysis/CFGBlock.cpp
@@ -5,75 +5,85 @@
 namespace weak {
 namespace frontEnd {
 
-void CFGBlock::Link(const std::shared_ptr<CFGBlock> &With) {
-  if (std::find(Successors.begin(), Successors.end(), With) != Successors.end())
-    DiagnosticError(0U, 0U) << "Given CFG block is already linked.";
-
-  AddSuccessor(std::move(With));
-  With->AddPredecessor(shared_from_this());
-}
-
-void CFGBlock::Unlink(const std::shared_ptr<CFGBlock> &With) {
-  auto FoundedBlock = std::find(Successors.begin(), Successors.end(), With);
-  if (FoundedBlock == Successors.end()) {
-    DiagnosticError(0U, 0U) << "Attempt to unlink non-existent CFG block.";
-    UnreachablePoint();
-  }
-
-  Successors.erase(FoundedBlock);
-  With->Predecessors.erase(
-      std::remove_if(With->Predecessors.begin(), With->Predecessors.end(),
-                     [this](const std::shared_ptr<CFGBlock> &BlockPtr) {
-                       return this == BlockPtr.get();
-                     }),
-      With->Predecessors.end());
-}
-
-bool CFGBlock::LinkedWith(const std::shared_ptr<CFGBlock> &Block) {
-  auto ThisLinked = std::find(Successors.begin(), Successors.end(), Block);
-  auto OtherLinked = std::find(Block->Predecessors.begin(),
-                               Block->Predecessors.end(), shared_from_this());
-
-  return (ThisLinked != Successors.end()) &&
-         (OtherLinked != Block->Predecessors.end());
-}
-
 void CFGBlock::AddStatement(std::unique_ptr<ASTNode> &&Statement) {
   /// Guaranteed unique object.
   Statements.push_back(std::move(Statement));
 }
 
-void CFGBlock::AddSuccessor(const std::shared_ptr<CFGBlock> &Block) {
-  if (std::find(Successors.begin(), Successors.end(), Block) ==
-      Successors.end()) {
-    Successors.push_back(std::move(Block));
-  } else {
-    DiagnosticError(0U, 0U) << "Given successor CFG is already linked.";
+void CFGBlock::AddSequentialSuccessor(const std::shared_ptr<CFGBlock> &Block) {
+  if (this->IsSuccessorOf(Block)) {
+    DiagnosticError(0U, 0U) << "CFG node is already linked as successor.";
     UnreachablePoint();
   }
+
+  Successors.first = Block;
+  Block->Predecessors.push_back(shared_from_this());
+}
+
+void CFGBlock::AddConditionSuccessors(const std::shared_ptr<CFGBlock> &Yes,
+                                      const std::shared_ptr<CFGBlock> &No) {
+  if (Successors.first == Yes || Successors.second == No) {
+    DiagnosticError(0U, 0U) << "CFG node is already linked as successor.";
+    UnreachablePoint();
+  }
+
+  Successors.first = Yes;
+  Successors.second = No;
+  Yes->Predecessors.push_back(shared_from_this());
+  No->Predecessors.push_back(shared_from_this());
 }
 
 void CFGBlock::AddPredecessor(const std::shared_ptr<CFGBlock> &Block) {
-  if (std::find(Predecessors.begin(), Predecessors.end(), Block) ==
+  if (std::find(Predecessors.begin(), Predecessors.end(), Block) !=
       Predecessors.end()) {
-    Predecessors.push_back(std::move(Block));
-  } else {
-    DiagnosticError(0U, 0U) << "Given predecessor CFG is already linked.";
+    DiagnosticError(0U, 0U) << "CFG node is already linked as predecessor.";
     UnreachablePoint();
   }
+
+  Predecessors.push_back(Block);
+  Block->Successors.first = shared_from_this();
+}
+
+bool CFGBlock::IsPredecessorOf(const std::shared_ptr<CFGBlock> &Block) {
+  return Successors.first == Block || Successors.second == Block;
+}
+
+bool CFGBlock::IsSuccessorOf(const std::shared_ptr<CFGBlock> &Block) {
+  return Block->Successors.first == shared_from_this() ||
+         Block->Successors.second == shared_from_this();
 }
 
 const std::vector<std::unique_ptr<ASTNode>> &CFGBlock::GetStatements() const {
   return Statements;
 }
 
-const std::vector<std::shared_ptr<CFGBlock>> &CFGBlock::GetSuccessors() const {
-  return Successors;
+const std::shared_ptr<CFGBlock> &CFGBlock::GetSequentialSuccessor() const {
+  return Successors.first;
+}
+
+const std::shared_ptr<CFGBlock> &CFGBlock::GetConditionalSuccessor() const {
+  return Successors.second;
 }
 
 const std::vector<std::shared_ptr<CFGBlock>> &
 CFGBlock::GetPredecessors() const {
   return Predecessors;
+}
+
+bool CFGBlock::IsFirstSuccessorPresent(
+    const std::shared_ptr<CFGBlock> &Block) const {
+  return Successors.first.get() == Block.get();
+}
+
+bool CFGBlock::IsSecondSuccessorPresent(
+    const std::shared_ptr<CFGBlock> &Block) const {
+  return Successors.second.get() == Block.get();
+}
+
+bool CFGBlock::IsPredecessorPresent(
+    const std::shared_ptr<CFGBlock> &Block) const {
+  return std::find(Predecessors.begin(), Predecessors.end(), Block) !=
+         Predecessors.end();
 }
 
 } // namespace frontEnd
