18690bf15be1bbf4ba037fbb8df26968595b1d99
epoll-reactor <>
2022-01-13 13:45:56 +0100

    Implement a three-address code IR.

--- compiler/src/MiddleEnd/CodeGen/CodeEmitter.cpp
+++ compiler/src/MiddleEnd/CodeGen/CodeEmitter.cpp
@@ -4,44 +4,18 @@
 namespace weak {
 namespace middleEnd {
 
-void CodeEmitter::Emit2Reg(Op Op, Reg Reg1, Reg Reg2) {
-  Instructions.push_back(TwoRegInstruction(Op, Reg1, Reg2));
-}
+CodeEmitter::CodeEmitter() : Instructions(), CurrentLabel(0U) {}
 
-void CodeEmitter::Emit2Reg(Op Op, Reg Reg1, signed Imm) {
-  Instructions.push_back(TwoRegInstruction(Op, Reg1, Imm));
-}
-
-void CodeEmitter::Emit1Reg(Op Op, Reg Reg) {
-  Instructions.push_back(OneRegInstruction(Op, Reg));
-}
-
-void CodeEmitter::EmitJumpInstr(signed Label) {
-  Instructions.push_back(JumpInstruction(Label));
-}
-
-void CodeEmitter::EmitJumpLabel(signed Label) {
-  Instructions.push_back(JumpLabel(Label));
+void CodeEmitter::Emit(frontEnd::TokenType Type,
+                       const Instruction::OperandVariant &Left,
+                       const Instruction::OperandVariant &Right) {
+  Instructions.emplace_back(CurrentLabel, Type, Left, Right);
+  ++CurrentLabel;
 }
 
 void CodeEmitter::Dump() {
-  struct InstrVisitor {
-    void operator()(const TwoRegInstruction &TwoReg) {
-      std::cout << TwoReg.Dump() << std::endl;
-    }
-    void operator()(const OneRegInstruction &OneReg) {
-      std::cout << OneReg.Dump() << std::endl;
-    }
-    void operator()(const JumpLabel &Jump) {
-      std::cout << Jump.Dump() << std::endl;
-    }
-    void operator()(const JumpInstruction &Jump) {
-      std::cout << Jump.Dump() << std::endl;
-    }
-  };
-
-  for (const auto &I : Instructions) {
-    std::visit(InstrVisitor{}, I);
+  for (const Instruction &I : Instructions) {
+    std::cout << I.Dump() << std::endl;
   }
 }
 
