6ea720ad6d4a6ad2dcb2cbd5d7f636159bc716da
epoll-reactor <>
2022-05-15 12:19:34 +0200

    Update HTML documentation.

--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -3,11 +3,10 @@ var searchData=
   ['record_0',['Record',['../structweak_1_1middleEnd_1_1Storage_1_1Record.html',1,'weak::middleEnd::Storage']]],
   ['recordmap_1',['RecordMap',['../classweak_1_1middleEnd_1_1Storage.html#ace880b4ce559ec75d43de781ae9f03ad',1,'weak::middleEnd::Storage']]],
   ['records_2',['Records',['../classweak_1_1middleEnd_1_1Storage.html#a41c855be854b9cd909afc99862371962',1,'weak::middleEnd::Storage']]],
-  ['reducegraph_3',['ReduceGraph',['../classweak_1_1middleEnd_1_1CFGBuilder.html#ad3ad6df5f757609da679c0aaa87a6197',1,'weak::middleEnd::CFGBuilder']]],
-  ['require_4',['Require',['../classweak_1_1frontEnd_1_1Parser.html#ac79effbd0b8f6020ab847081d16ab1f8',1,'weak::frontEnd::Parser::Require(const std::vector&lt; TokenType &gt; &amp;Expected)'],['../classweak_1_1frontEnd_1_1Parser.html#a3ca31d26f3e6951385895627e5a6b200',1,'weak::frontEnd::Parser::Require(TokenType Expected)']]],
-  ['return_5',['RETURN',['../namespaceweak_1_1frontEnd.html#a7999f2df7361fe8f6c75e1e68f25b649aa2bec276a54439fe011eb523b845dac5',1,'weak::frontEnd']]],
-  ['return_5fstmt_6',['RETURN_STMT',['../namespaceweak_1_1frontEnd.html#a0a382c2a6f386a8773b485076c2f2abba9eeeea6ff7f6f2e3a6b03baf772e159e',1,'weak::frontEnd']]],
-  ['returntype_7',['ReturnType',['../classweak_1_1frontEnd_1_1ASTFunctionDecl.html#ab050fa0109f7631bb0d3b727767b1baf',1,'weak::frontEnd::ASTFunctionDecl']]],
-  ['rhs_8',['RHS',['../classweak_1_1frontEnd_1_1ASTBinaryOperator.html#a967e7792b7d4837e9193ea2248ba733f',1,'weak::frontEnd::ASTBinaryOperator']]],
-  ['rootnode_9',['RootNode',['../classweak_1_1middleEnd_1_1IRNodePrinter.html#a1961996ecf01585c171e7557e9161932',1,'weak::middleEnd::IRNodePrinter::RootNode()'],['../classanonymous__namespace_02ASTPrettyPrint_8cpp_03_1_1ASTPrintVisitor.html#ac57bbb8488269b12dde4c6e133645e87',1,'anonymous_namespace{ASTPrettyPrint.cpp}::ASTPrintVisitor::RootNode()']]]
+  ['require_3',['Require',['../classweak_1_1frontEnd_1_1Parser.html#ac79effbd0b8f6020ab847081d16ab1f8',1,'weak::frontEnd::Parser::Require(const std::vector&lt; TokenType &gt; &amp;Expected)'],['../classweak_1_1frontEnd_1_1Parser.html#a3ca31d26f3e6951385895627e5a6b200',1,'weak::frontEnd::Parser::Require(TokenType Expected)']]],
+  ['return_4',['RETURN',['../namespaceweak_1_1frontEnd.html#a7999f2df7361fe8f6c75e1e68f25b649aa2bec276a54439fe011eb523b845dac5',1,'weak::frontEnd']]],
+  ['return_5fstmt_5',['RETURN_STMT',['../namespaceweak_1_1frontEnd.html#a0a382c2a6f386a8773b485076c2f2abba9eeeea6ff7f6f2e3a6b03baf772e159e',1,'weak::frontEnd']]],
+  ['returntype_6',['ReturnType',['../classweak_1_1frontEnd_1_1ASTFunctionDecl.html#ab050fa0109f7631bb0d3b727767b1baf',1,'weak::frontEnd::ASTFunctionDecl']]],
+  ['rhs_7',['RHS',['../classweak_1_1frontEnd_1_1ASTBinaryOperator.html#a967e7792b7d4837e9193ea2248ba733f',1,'weak::frontEnd::ASTBinaryOperator']]],
+  ['rootnode_8',['RootNode',['../classweak_1_1middleEnd_1_1IRNodePrinter.html#a1961996ecf01585c171e7557e9161932',1,'weak::middleEnd::IRNodePrinter::RootNode()'],['../classanonymous__namespace_02ASTPrettyPrint_8cpp_03_1_1ASTPrintVisitor.html#ac57bbb8488269b12dde4c6e133645e87',1,'anonymous_namespace{ASTPrettyPrint.cpp}::ASTPrintVisitor::RootNode()']]]
 ];
--- docs/search/all_f.js
+++ docs/search/all_f.js
@@ -2,15 +2,14 @@ var searchData=
 [
   ['temporarylabel_0',['TemporaryLabel',['../structweak_1_1middleEnd_1_1Storage_1_1Record.html#ab29ea7cd415d0f6d908b5cb7d448a8a1',1,'weak::middleEnd::Storage::Record']]],
   ['thenbody_1',['ThenBody',['../classweak_1_1frontEnd_1_1ASTIfStmt.html#a966cf1b476deb142842a7286b1d2f23e',1,'weak::frontEnd::ASTIfStmt']]],
-  ['todo_20list_2',['Todo List',['../todo.html',1,'']]],
-  ['token_3',['Token',['../structweak_1_1frontEnd_1_1Token.html#a08fdca0461445495a71d0879bc315763',1,'weak::frontEnd::Token::Token()'],['../structweak_1_1frontEnd_1_1Token.html',1,'weak::frontEnd::Token']]],
-  ['token_2ecpp_4',['Token.cpp',['../Token_8cpp.html',1,'']]],
-  ['token_2ehpp_5',['Token.hpp',['../Token_8hpp.html',1,'']]],
-  ['tokenstostring_6',['TokensToString',['../Parser_8cpp.html#aec5d39c40e62b5d96b90b8ec18919952',1,'Parser.cpp']]],
-  ['tokentostring_7',['TokenToString',['../namespaceweak_1_1frontEnd.html#aeacd5dac29c0d96c7ba1e455339579b7',1,'weak::frontEnd']]],
-  ['tokentype_8',['TokenType',['../namespaceweak_1_1frontEnd.html#a7999f2df7361fe8f6c75e1e68f25b649',1,'weak::frontEnd::TokenType()'],['../Lexer_8cpp.html#a91b4731cc2cbb2bec396a45349943611',1,'TokenType():&#160;Lexer.cpp']]],
-  ['totalvariables_9',['TotalVariables',['../classweak_1_1middleEnd_1_1Storage.html#a248ee47916b197fd09c173ce906cf1f5',1,'weak::middleEnd::Storage']]],
-  ['true_10',['TRUE',['../namespaceweak_1_1frontEnd.html#a7999f2df7361fe8f6c75e1e68f25b649ac0d83f0b82a6b30de8811e69e6d95c61',1,'weak::frontEnd']]],
-  ['truebranch_11',['TrueBranch',['../classweak_1_1middleEnd_1_1IRBranch.html#a9ffc644c3e95e4d3e93271753099ea55',1,'weak::middleEnd::IRBranch']]],
-  ['type_12',['Type',['../structweak_1_1frontEnd_1_1Token.html#a89700cad75f13b4056bca370825e2610',1,'weak::frontEnd::Token::Type()'],['../classweak_1_1middleEnd_1_1IRNode.html#a213ed1c96c92f1bb8e52539384df50e5',1,'weak::middleEnd::IRNode::Type()']]]
+  ['token_2',['Token',['../structweak_1_1frontEnd_1_1Token.html#a08fdca0461445495a71d0879bc315763',1,'weak::frontEnd::Token::Token()'],['../structweak_1_1frontEnd_1_1Token.html',1,'weak::frontEnd::Token']]],
+  ['token_2ecpp_3',['Token.cpp',['../Token_8cpp.html',1,'']]],
+  ['token_2ehpp_4',['Token.hpp',['../Token_8hpp.html',1,'']]],
+  ['tokenstostring_5',['TokensToString',['../Parser_8cpp.html#aec5d39c40e62b5d96b90b8ec18919952',1,'Parser.cpp']]],
+  ['tokentostring_6',['TokenToString',['../namespaceweak_1_1frontEnd.html#aeacd5dac29c0d96c7ba1e455339579b7',1,'weak::frontEnd']]],
+  ['tokentype_7',['TokenType',['../namespaceweak_1_1frontEnd.html#a7999f2df7361fe8f6c75e1e68f25b649',1,'weak::frontEnd::TokenType()'],['../Lexer_8cpp.html#a91b4731cc2cbb2bec396a45349943611',1,'TokenType():&#160;Lexer.cpp']]],
+  ['totalvariables_8',['TotalVariables',['../classweak_1_1middleEnd_1_1Storage.html#a248ee47916b197fd09c173ce906cf1f5',1,'weak::middleEnd::Storage']]],
+  ['true_9',['TRUE',['../namespaceweak_1_1frontEnd.html#a7999f2df7361fe8f6c75e1e68f25b649ac0d83f0b82a6b30de8811e69e6d95c61',1,'weak::frontEnd']]],
+  ['truebranch_10',['TrueBranch',['../classweak_1_1middleEnd_1_1IRBranch.html#a9ffc644c3e95e4d3e93271753099ea55',1,'weak::middleEnd::IRBranch']]],
+  ['type_11',['Type',['../structweak_1_1frontEnd_1_1Token.html#a89700cad75f13b4056bca370825e2610',1,'weak::frontEnd::Token::Type()'],['../classweak_1_1middleEnd_1_1IRNode.html#a213ed1c96c92f1bb8e52539384df50e5',1,'weak::middleEnd::IRNode::Type()']]]
 ];
