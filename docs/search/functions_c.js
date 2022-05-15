456c50b38cc67a02032a3e710926b19a22931351
epoll-reactor <>
2022-05-15 12:19:34 +0200

    Update HTML documentation.

--- docs/search/functions_c.js
+++ docs/search/functions_c.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['reducegraph_0',['ReduceGraph',['../classweak_1_1middleEnd_1_1CFGBuilder.html#ad3ad6df5f757609da679c0aaa87a6197',1,'weak::middleEnd::CFGBuilder']]],
-  ['require_1',['Require',['../classweak_1_1frontEnd_1_1Parser.html#ac79effbd0b8f6020ab847081d16ab1f8',1,'weak::frontEnd::Parser::Require(const std::vector&lt; TokenType &gt; &amp;Expected)'],['../classweak_1_1frontEnd_1_1Parser.html#a3ca31d26f3e6951385895627e5a6b200',1,'weak::frontEnd::Parser::Require(TokenType Expected)']]]
+  ['require_0',['Require',['../classweak_1_1frontEnd_1_1Parser.html#ac79effbd0b8f6020ab847081d16ab1f8',1,'weak::frontEnd::Parser::Require(const std::vector&lt; TokenType &gt; &amp;Expected)'],['../classweak_1_1frontEnd_1_1Parser.html#a3ca31d26f3e6951385895627e5a6b200',1,'weak::frontEnd::Parser::Require(TokenType Expected)']]]
 ];
--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -9,8 +9,7 @@ var indexSectionsWithContent =
   6: "rt",
   7: "adntu",
   8: "abcdefgilmnoprstvwx",
-  9: "c",
-  10: "t"
+  9: "c"
 };
 
 var indexSectionNames =
@@ -24,8 +23,7 @@ var indexSectionNames =
   6: "typedefs",
   7: "enums",
   8: "enumvalues",
-  9: "related",
-  10: "pages"
+  9: "related"
 };
 
 var indexSectionLabels =
@@ -39,7 +37,6 @@ var indexSectionLabels =
   6: "Typedefs",
   7: "Enumerations",
   8: "Enumerator",
-  9: "Friends",
-  10: "Pages"
+  9: "Friends"
 };
 
