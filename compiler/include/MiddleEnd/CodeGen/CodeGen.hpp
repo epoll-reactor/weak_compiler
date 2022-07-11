22db2fc4e17856b6d449303fd94c9585a1317697
Davyd Karanchuk <Davyd.Karanchuk@harman.com>
2022-07-11 15:14:53 +0200

    Add LICENSE notes.

--- compiler/include/MiddleEnd/CodeGen/CodeGen.hpp
+++ compiler/include/MiddleEnd/CodeGen/CodeGen.hpp
@@ -1,3 +1,9 @@
+/* CodeGen.hpp - LLVM IR generator.
+ * Copyright (C) 2022 epoll-reactor <glibcxx.chrono@gmail.com>
+ *
+ * This file is distributed under the MIT license.
+ */
+
 #ifndef WEAK_COMPILER_MIDDLE_END_CODEGEN_HPP
 #define WEAK_COMPILER_MIDDLE_END_CODEGEN_HPP
 
